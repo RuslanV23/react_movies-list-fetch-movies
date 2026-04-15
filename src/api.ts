a28c30203adf5593edc5627e77de0defd157ac43
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-15 17:42:54 +0300

    add fix

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,16 @@ const KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL + KEY}&t=${encodeURIComponent(query)}`)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        return {
+          Response: 'False',
+          Error: res.statusText || 'unexpected error',
+        } as ResponseError;
+      }
+
+      return res.json();
+    })
     .catch(() => ({
       Response: 'False',
       Error: 'unexpected error',
